da0fc65b98b6886db913aaff61930e800b9bacdd
Abdullah Akbar <Akbar.A@etoncollege.org.uk>
2023-01-19 17:02:15 +0000

    Initial Commit

---
+++ NewCarCustomiser/NewCarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewCarCustomiserApp.swift
+//  NewCarCustomiser
+//
+//  Created by Akbar, Abdullah (RCH) on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewCarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
